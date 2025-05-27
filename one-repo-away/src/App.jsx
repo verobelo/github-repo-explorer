322753e95a2ffe793f8fefd62c6ed6b6138d53aa
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-27 23:42:27 +0200

    Add basic layout

--- one-repo-away/src/App.jsx
+++ one-repo-away/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "@chakra-ui/react/container";
+import { VStack } from "@chakra-ui/react/stack";
+import { Box } from "@chakra-ui/react/box";
+import Searchbar from "./components/SearchBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container maxW={"3xl"} p={"0"} minH={"100dvh"}>
+      <VStack p={"4"} gap={"4"}>
+        <Header />
+        <Box as="main">
+          <Searchbar />
+        </Box>
+        <Footer />
+      </VStack>
+    </Container>
+  );
 }
 
-export default App
+export default App;
