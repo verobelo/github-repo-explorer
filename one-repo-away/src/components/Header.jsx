03b03e61f5a6ebf4b295c58163ba9881830ce977
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-28 16:08:26 +0200

    Increase font sizes for tablets

--- one-repo-away/src/components/Header.jsx
+++ one-repo-away/src/components/Header.jsx
@@ -1,17 +1,20 @@
 import { Heading, Text } from "@chakra-ui/react/typography";
 import { ColorModeButton } from "./ui/color-mode";
 import { Flex } from "@chakra-ui/react/flex";
+import { Box } from "@chakra-ui/react/box";
 
 export default function Header() {
   return (
     <header style={{ textAlign: "center" }}>
-      <Flex align={"center"} justify={{ base: "space-around" }}>
-        <Heading as={"h1"} textStyle={{ base: "2xl" }}>
+      <Flex align={"center"} justify="center" gap="8">
+        <Heading as={"h1"} textStyle={{ base: "2xl", md: "3xl" }}>
           One Repo Away.
         </Heading>
-        <ColorModeButton />
+        <Box bg={{ _light: "gray.100", _dark: "transparent" }} rounded={"2xl"}>
+          <ColorModeButton />
+        </Box>
       </Flex>
-      <Text textStyle={{ base: "xs" }} fontStyle={"italic"}>
+      <Text textStyle={{ base: "xs", sm: "sm", md: "md" }} fontStyle={"italic"}>
         The perfect GitHub repo is just one search away.
       </Text>
     </header>
