e08a585b9c01bf8598623c002dbe45898884b896
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-28 10:47:09 +0200

    Add general layout

--- one-repo-away/src/components/SearchBar.jsx
+++ one-repo-away/src/components/SearchBar.jsx
@@ -1,23 +1,24 @@
 import { Box } from "@chakra-ui/react/box";
-import { IconButton } from "@chakra-ui/react/button";
+import { Button, IconButton } from "@chakra-ui/react/button";
 import { Flex } from "@chakra-ui/react/flex";
 import { Input } from "@chakra-ui/react/input";
 import { Select } from "@chakra-ui/react/select";
 import { Portal } from "@chakra-ui/react/portal";
 import { languages } from "@/logic/languages";
 import { LuSearch } from "react-icons/lu";
+import { Group } from "@chakra-ui/react/group";
 
 export default function Searchbar() {
   return (
     <Box>
-      <Flex align={"center"}>
+      <Group attached w={"full"}>
         <Input type="text" placeholder="Search repositories..." size={"sm"} />
         <IconButton colorPalette={"blue"} size={"sm"}>
           <LuSearch />
         </IconButton>
-      </Flex>
+      </Group>
 
-      <Flex>
+      <Flex direction={{ base: "column" }}>
         <Select.Root collection={languages} size="sm">
           <Select.HiddenSelect />
           <Select.Control>
@@ -41,6 +42,29 @@ export default function Searchbar() {
             </Select.Positioner>
           </Portal>
         </Select.Root>
+
+        <Select.Root size="sm">
+          <Select.HiddenSelect />
+          <Select.Control>
+            <Select.Trigger>
+              <Select.ValueText placeholder="Filter by" />
+            </Select.Trigger>
+            <Select.IndicatorGroup>
+              <Select.Indicator />
+            </Select.IndicatorGroup>
+          </Select.Control>
+          <Portal>
+            <Select.Positioner>
+              <Select.Content>
+                <Select.Item item="Best Match">Best Match</Select.Item>
+                <Select.Item item="Most Stars">Most Stars</Select.Item>
+                <Select.Item item="Most Forks">Most Forks</Select.Item>
+              </Select.Content>
+            </Select.Positioner>
+          </Portal>
+        </Select.Root>
+
+        <Button colorPalette={"green"}>Find a Random Repo</Button>
       </Flex>
     </Box>
   );
