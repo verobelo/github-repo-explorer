527f36c0e6dad8ffd38b2e8013442e272deb2c3f
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-28 15:50:32 +0200

    Add general app layout

--- one-repo-away/src/App.jsx
+++ one-repo-away/src/App.jsx
@@ -4,14 +4,18 @@ import { Container } from "@chakra-ui/react/container";
 import { VStack } from "@chakra-ui/react/stack";
 import { Box } from "@chakra-ui/react/box";
 import Searchbar from "./components/SearchBar";
+import RepoCardContainer from "./components/RepoCardContainer";
 
 function App() {
   return (
     <Container maxW={"3xl"} p={"0"} minH={"100dvh"}>
-      <VStack p={"4"} gap={"4"}>
+      <VStack p={"4"} gap={"5"} align={"stretch"}>
         <Header />
         <Box as="main">
-          <Searchbar />
+          <VStack gap="5" align="stretch">
+            <Searchbar />
+            <RepoCardContainer />
+          </VStack>
         </Box>
         <Footer />
       </VStack>
