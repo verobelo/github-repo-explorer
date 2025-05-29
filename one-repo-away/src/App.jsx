7a4a22f7e11136e7e019160f7dadf092996ab8e2
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-29 21:11:01 +0200

    Add state and fetching logic for getting repos

--- one-repo-away/src/App.jsx
+++ one-repo-away/src/App.jsx
@@ -5,16 +5,44 @@ import { VStack } from "@chakra-ui/react/stack";
 import { Box } from "@chakra-ui/react/box";
 import Searchbar from "./components/SearchBar";
 import RepoCardContainer from "./components/RepoCardContainer";
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [repos, setRepos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSearch = async () => {
+    setIsLoading(true);
+    setError("");
+    try {
+      const res = await fetch(
+        `https://api.github.com/search/repositories?q=${encodeURIComponent(
+          query
+        )}&sort=stars&order=desc`
+      );
+      const data = await res.json();
+      setRepos(data.items || []);
+    } catch (err) {
+      setError(err.message || "Something went wrong.Try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Container maxW={{ base: "3xl" }} p={"0"} minH={"100dvh"}>
       <VStack p={"4"} gap={"6"} align={{ base: "stretch", md: "center" }}>
         <Header />
         <Box as="main">
           <VStack gap="6" align="stretch" mt={{ base: 2, md: 4 }}>
-            <Searchbar />
-            <RepoCardContainer />
+            <Searchbar
+              query={query}
+              setQuery={setQuery}
+              onSearch={handleSearch}
+            />
+            <RepoCardContainer repos={repos} />
           </VStack>
         </Box>
         <Footer />
