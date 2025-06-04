63ed62247c1518d2ba08ee591a1eafbba367456a
verobelo <142001535+verobelo@users.noreply.github.com>
2025-06-04 12:17:16 +0200

    Add marginTop

--- one-repo-away/src/components/Footer.jsx
+++ one-repo-away/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
-    <Box as="footer" textAlign="center" p={"2"}>
+    <Box as="footer" textAlign="center" p={"2"} marginTop="auto">
       <Text fontSize={{ base: "xs", sm: "sm", md: "md" }}>
         &copy; 2025 Veronika Belozerova. All rights reserved.
       </Text>
