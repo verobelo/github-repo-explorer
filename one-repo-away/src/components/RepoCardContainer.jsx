ce7845d2ce623d107ecf77cc7f2559578ef6fa10
verobelo <142001535+verobelo@users.noreply.github.com>
2025-06-06 13:50:20 +0200

    Add tabs and favorites logic

--- one-repo-away/src/components/RepoCardContainer.jsx
+++ one-repo-away/src/components/RepoCardContainer.jsx
@@ -5,6 +5,7 @@ import { Pagination } from "@chakra-ui/react/pagination";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Box } from "@chakra-ui/react/box";
 import { Heading } from "@chakra-ui/react/typography";
+import { Tabs } from "@chakra-ui/react/tabs";
 
 export default function RepoCardContainer({
   repos,
@@ -14,7 +15,11 @@ export default function RepoCardContainer({
   totalResults,
   totalPages,
   randomRepoId,
+  handleToggleFavorite,
+  favoriteRepos,
 }) {
+  console.log("handleToggleFavorite exists?", typeof handleToggleFavorite);
+
   return (
     <Box
       bg={{ base: "gray.50", _dark: "gray.800" }}
@@ -23,91 +28,142 @@ export default function RepoCardContainer({
       border="1px solid"
       borderColor={{ base: "gray.200", _dark: "gray.700" }}
       boxShadow="sm">
-      <Heading
-        as="h2"
-        fontSize={{ base: "sm", md: "md", xl: "lg" }}
-        pb={"1"}
-        color={{ _light: "gray.600", _dark: "gray.400" }}
-        textAlign={"center"}>
-        {totalResults === 0 && "Repository Results"}
-        {totalResults === 1 && "Random Repository"}
-        {totalResults > 1 &&
-          totalResults <= 1000 &&
-          `Repository results (${Math.min(
-            totalResults,
-            perPage * totalPages
-          )})`}
+      <Tabs.Root defaultValue="results">
+        <Tabs.List>
+          <Tabs.Trigger value="results">Search Results</Tabs.Trigger>
+          <Tabs.Trigger value="favorite">Favorite</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="results">
+          <Heading
+            as="h2"
+            fontSize={{ base: "sm", md: "md", xl: "lg" }}
+            pb={"1"}
+            color={{ _light: "gray.600", _dark: "gray.400" }}
+            textAlign={"center"}>
+            {totalResults === 0 && "Repository Results"}
+            {totalResults === 1 && "Random Repository"}
+            {totalResults > 1 &&
+              totalResults <= 1000 &&
+              `Repository results (${Math.min(
+                totalResults,
+                perPage * totalPages
+              )})`}
 
-        {totalResults > 1000 &&
-          `Showing top ${Math.min(
-            totalResults,
-            perPage * totalPages
-          )} repositories (as per GitHub REST API limitations)`}
-      </Heading>
-      <Grid
-        as={"ul"}
-        w={"full"}
-        gridTemplateColumns={{
-          base: "minmax(0, 1fr)",
-          md: "repeat(2, minmax(300px, 1fr))",
-          xl: "repeat(3, minmax(300px, 1fr))",
-        }}
-        placeContent={"center"}
-        gap={"2"}>
-        {repos.map((repo) => (
-          <GridItem key={repo.id} w={"full"} maxW="300px">
-            <RepoCard
-              title={repo.full_name}
-              description={repo.description}
-              language={repo.language}
-              forks={repo.forks}
-              stars={repo.stargazers_count}
-              url={repo.html_url}
-              isRandom={repo.id === randomRepoId}
-            />
-          </GridItem>
-        ))}
-      </Grid>
-      {repos.length > 0 && (
-        <Box w={"full"} display={"flex"} justifyContent={"center"}>
-          <Pagination.Root
-            count={perPage * totalPages}
-            pageSize={perPage}
-            aria-label="pagination"
-            p="2"
-            siblingCount={"3"}
-            page={page}
-            onPageChange={(e) => goToPage(e.page)}>
-            <ButtonGroup
-              attached
-              variant="ghost"
-              size={{ base: "xs", sm: "md", lg: "lg" }}>
-              <Pagination.PrevTrigger asChild>
-                <IconButton aria-label="Go to previous">
-                  <ChevronLeft />
-                </IconButton>
-              </Pagination.PrevTrigger>
+            {totalResults > 1000 &&
+              `Showing top ${Math.min(
+                totalResults,
+                perPage * totalPages
+              )} repositories (as per GitHub REST API limitations)`}
+          </Heading>
+          <Grid
+            as={"ul"}
+            w={"full"}
+            gridTemplateColumns={{
+              base: "minmax(0, 1fr)",
+              md: "repeat(2, minmax(300px, 1fr))",
+              xl: "repeat(3, minmax(300px, 1fr))",
+            }}
+            placeContent={"center"}
+            gap={"2"}>
+            {repos.map((repo) => (
+              <GridItem key={repo.id} w={"full"} maxW="300px">
+                <RepoCard
+                  repo={repo}
+                  title={repo.full_name}
+                  description={repo.description}
+                  language={repo.language}
+                  forks={repo.forks}
+                  stars={repo.stargazers_count}
+                  url={repo.html_url}
+                  isRandom={repo.id === randomRepoId}
+                  isFavorite={favoriteRepos.some((r) => r.id === repo.id)}
+                  onToggleFavorite={handleToggleFavorite}
+                />
+              </GridItem>
+            ))}
+          </Grid>
+          {repos.length > 0 && (
+            <Box w={"full"} display={"flex"} justifyContent={"center"}>
+              <Pagination.Root
+                count={perPage * totalPages}
+                pageSize={perPage}
+                aria-label="pagination"
+                p="2"
+                siblingCount={"3"}
+                page={page}
+                onPageChange={(e) => goToPage(e.page)}>
+                <ButtonGroup
+                  attached
+                  variant="ghost"
+                  size={{ base: "xs", sm: "md", lg: "lg" }}>
+                  <Pagination.PrevTrigger asChild>
+                    <IconButton aria-label="Go to previous">
+                      <ChevronLeft />
+                    </IconButton>
+                  </Pagination.PrevTrigger>
 
-              <Pagination.Items
-                render={(page) => (
-                  <IconButton
-                    key={page.value}
-                    {...page}
-                    variant={{ base: "ghost", _selected: "outline" }}>
-                    {page.value}
-                  </IconButton>
-                )}
-              />
+                  <Pagination.Items
+                    render={(page) => (
+                      <IconButton
+                        key={page.value}
+                        {...page}
+                        variant={{ base: "ghost", _selected: "outline" }}>
+                        {page.value}
+                      </IconButton>
+                    )}
+                  />
 
-              <Pagination.NextTrigger asChild>
-                <IconButton aria-label="Go to next">
-                  <ChevronRight />
-                </IconButton>
-              </Pagination.NextTrigger>
-            </ButtonGroup>
-          </Pagination.Root>
-        </Box>
-      )}
+                  <Pagination.NextTrigger asChild>
+                    <IconButton aria-label="Go to next">
+                      <ChevronRight />
+                    </IconButton>
+                  </Pagination.NextTrigger>
+                </ButtonGroup>
+              </Pagination.Root>
+            </Box>
+          )}
+        </Tabs.Content>
+        <Tabs.Content value="favorite">
+          <Box maxH={{ base: "60vh", md: "70vh" }} overflowY="auto" pr={"2"}>
+            {" "}
+            <Heading
+              as="h2"
+              fontSize={{ base: "sm", md: "md", xl: "lg" }}
+              pb={"1"}
+              color={{ _light: "gray.600", _dark: "gray.400" }}
+              textAlign={"center"}>
+              Your Favorite Repositories
+            </Heading>
+            <Grid
+              as={"ul"}
+              w={"full"}
+              gridTemplateColumns={{
+                base: "minmax(0, 1fr)",
+                md: "repeat(2, minmax(300px, 1fr))",
+                xl: "repeat(3, minmax(300px, 1fr))",
+              }}
+              placeContent={"center"}
+              gap={"2"}>
+              {favoriteRepos.map((repo) => (
+                <GridItem key={repo.id} w={"full"} maxW="300px">
+                  <RepoCard
+                    repo={repo}
+                    title={repo.full_name}
+                    description={repo.description}
+                    language={repo.language}
+                    forks={repo.forks}
+                    stars={repo.stargazers_count}
+                    url={repo.html_url}
+                    isRandom={repo.id === randomRepoId}
+                    isFavorite={favoriteRepos.some((r) => r.id === repo.id)}
+                    onToggleFavorite={handleToggleFavorite}
+                  />
+                </GridItem>
+              ))}
+            </Grid>
+          </Box>
+        </Tabs.Content>
+      </Tabs.Root>
     </Box>
   );
 }
